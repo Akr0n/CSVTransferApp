24f789235aca4f3ab9a171f17eb805c73ee174dc
Akr0n <federico.bot@gmail.com>
2025-10-01 08:48:22 +0200

    fix logiche di business

--- src/CSVTransferApp.Core/Interfaces/Infrastructure.cs
+++ src/CSVTransferApp.Core/Interfaces/Infrastructure.cs
@@ -15,24 +15,6 @@ public interface ICredentialManager
     void SaveCredential(string key, string value);
 }
 
-public interface IDataAdapterFactory
-{
-    /// <summary>
-    /// Crea un data adapter per il provider specificato
-    /// </summary>
-    /// <param name="providerName">Nome del provider database</param>
-    /// <param name="command">Comando da associare al data adapter</param>
-    /// <returns>Data adapter specifico per il provider</returns>
-    IDbDataAdapter CreateDataAdapter(string providerName, IDbCommand command);
-
-    /// <summary>
-    /// Verifica se un provider è supportato
-    /// </summary>
-    /// <param name="providerName">Nome del provider da verificare</param>
-    /// <returns>true se il provider è supportato, false altrimenti</returns>
-    bool IsProviderSupported(string providerName);
-}
-
 public interface IDatabaseConnectionManager
 {
     Task<IDbConnection> GetConnectionAsync(string connectionName);
